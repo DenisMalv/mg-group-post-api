8771134550a889bf2f866e5b4dcd6b7006601699
Denis Malniev <kissed03@gmail.com>
2025-03-11 19:21:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res) => {
 });
 
 app.use((err, req, res, next) => {
-	console.log(error);
+	console.log(err);
 	const { status = 500, message = "Server Error" } = err;
 	res.status(status).json({ message: message });
 });
