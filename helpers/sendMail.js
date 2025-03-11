689e2c28c6007de6591d89f6eeb982f9b1efaa3f
Denis Malniev <kissed03@gmail.com>
2025-03-11 19:35:29 +0200

    upd to mggroup meta email

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,21 +1,21 @@
-const nodemailer = require('nodemailer')
-require('dotenv').config()
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
-const {META_PASSWORD} = process.env
+const { META_PASSWORD } = process.env;
 
 const nodemailerConfig = {
-    host: "smtp.meta.ua",                
-    port: 465,             // 25,465, 2525
-    secure:true,
-    auth:{
-        user: "kissed03@meta.ua",          // my-createTestAccount(mail)-in-meta-ua dev.
-        // pass: META_PASSWORD                // dev.
-        // user: "mvt_mail_agent_one@meta.ua",          // customer(mail)-in-meta-ua
-        pass: META_PASSWORD
-    }
-}
+	host: "smtp.meta.ua",
+	port: 465, // 25,465, 2525
+	secure: true,
+	auth: {
+		// user: "kissed03@meta.ua",          // my-createTestAccount(mail)-in-meta-ua dev.
+		// pass: META_PASSWORD                // dev.
+		user: "mgbeton@meta.ua", // customer(mail)-in-meta-ua
+		pass: META_PASSWORD,
+	},
+};
 
-const transport = nodemailer.createTransport(nodemailerConfig)
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 // const email = {
 //     to: 'reciever',
@@ -24,11 +24,11 @@ const transport = nodemailer.createTransport(nodemailerConfig)
 //     html: '<p>Test mail from my server</p>'
 // }
 
-const sendEmail = async(data) =>{
-    // const email = {...data, from:'MVT BEARINGS LANDING kissed03@meta.ua'} //dev.
-    const email = {...data, from:'mg-group - kissed03@meta.ua'}  //customer
-    await transport.sendMail(email)
-    return true
-}
+const sendEmail = async (data) => {
+	// const email = {...data, from:'MVT BEARINGS LANDING kissed03@meta.ua'} //dev.
+	const email = { ...data, from: "mg-group - kissed03@meta.ua" }; //customer
+	await transport.sendMail(email);
+	return true;
+};
 
-module.exports = sendEmail
+module.exports = sendEmail;
